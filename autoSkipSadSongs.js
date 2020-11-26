cf8f257a69cac3fb4b4f2f2c24788be40e1399a8
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-11-25 23:29:47 -0800

    Change spacing in autoSkipSadSongs

--- autoSkipSadSongs.js
+++ autoSkipSadSongs.js
@@ -9,14 +9,14 @@
         const data = Spicetify.Player.data || Spicetify.Queue;
         if (!data) return;
 
-		// Get title and artist name
+        // Get title and artist name
         const artist = data.track.metadata.artist_name;
         const title = data.track.metadata.title;
 
-		const sad_artists = ["AJR", "Sleeping At Last", "Of Monsters and Men"];
-		const sad_titles = ["Lost Stars", "Had It All"];
+        const sad_artists = ["AJR", "Sleeping At Last", "Of Monsters and Men"];
+        const sad_titles = ["Lost Stars", "Had It All"];
         // If the artist name or title are in their corresponding arrays
-		if (sad_artists.includes(artist) || sad_titles.includes(title)) {
+        if (sad_artists.includes(artist) || sad_titles.includes(title)) {
             Spicetify.Player.next();
         }
     });
